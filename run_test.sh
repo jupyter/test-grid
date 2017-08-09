59ac11bfca69616d490c6c36a644b47877f6e3e2
Min RK <benjaminrk@gmail.com>
2017-08-09 17:32:52 +0200

    add default test command

--- run_test.sh
+++ run_test.sh
@@ -3,4 +3,8 @@ set -ex
 source ./bashrc
 cd $TEST
 test -f requirements.txt && pip install --upgrade -r requirements.txt
-. test.sh
+if [ -f test.sh ]; then
+    . test.sh
+else
+    . ../default-test.sh
+fi
--- traitlets-master-downstream-stable/test.sh
+++ traitlets-master-downstream-stable/test.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 install_ip traitlets
-py.test --pyargs traitlets
-py.test --pyargs jupyter_core
-iptest
-pytest -v --pyargs nbformat
-pytest -v --pyargs jupyter_client
-nosetests -v ipykernel
-pytest -v --pyargs nbconvert
-nosetests -v notebook
-nosetests -v ipywidgets
+
+. ../default-test.sh
