9248157b918d484923f2ae6222722590ec10e066
Min RK <benjaminrk@gmail.com>
2017-08-10 14:45:32 +0200

    run all tests before exiting with failure

--- default/test.sh
+++ default/test.sh
@@ -2,13 +2,18 @@
 
 # default tests to run (runs the test command for every package)
 # if no test.sh is defined, this will run instead
+set +e
 
-py.test --pyargs traitlets
-py.test --pyargs jupyter_core
-iptest
-pytest --pyargs nbformat
-pytest --pyargs jupyter_client
-nosetests ipykernel
-pytest --pyargs nbconvert
-nosetests notebook
-nosetests ipywidgets
+rc=0
+
+py.test --pyargs traitlets || rc=1
+py.test --pyargs jupyter_core || rc=1
+iptest || rc=1
+pytest --pyargs nbformat || rc=1
+pytest --pyargs jupyter_client || rc=1
+nosetests ipykernel || rc=1
+pytest --pyargs nbconvert || rc=1
+nosetests notebook || rc=1
+nosetests ipywidgets || rc=1
+
+exit $rc
