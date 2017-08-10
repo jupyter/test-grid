5ddb5810b60d2a40d1a6caa01fcca94a102b0566
Min RK <benjaminrk@gmail.com>
2017-08-10 09:58:47 +0200

    no-v in default-test

--- default-test.sh
+++ default-test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # default tests to run (runs the test command for every package)
 # use this if the default tests are what you want
-py.test -v --pyargs traitlets
-py.test -v --pyargs jupyter_core
-iptest -- -v
-pytest -v --pyargs nbformat
-pytest -v --pyargs jupyter_client
-nosetests -v ipykernel
-pytest -v --pyargs nbconvert
-nosetests -v notebook
-nosetests -v ipywidgets
+py.test --pyargs traitlets
+py.test --pyargs jupyter_core
+iptest
+pytest --pyargs nbformat
+pytest --pyargs jupyter_client
+nosetests ipykernel
+pytest --pyargs nbconvert
+nosetests notebook
+nosetests ipywidgets
